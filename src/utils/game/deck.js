4a4330d5333a846c1f2529ea428e290dc55f2020
bob <dawid_kol@icloud.com>
2025-10-31 14:23:14 +0100

    a;;;;

--- src/utils/game/deck.js
+++ src/utils/game/deck.js
@@ -14,3 +14,16 @@ function createDeck() {
     }
     return deck
 }
+
+function shuffleDeck(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[deck[i], deck[j]] = [deck[j], deck[i]]
+    }
+    return deck
+}
+
+
+
+
+export { createDeck, shuffleDeck }
